a0f5832cd1d9afb49ce33d5979617e06e0a5e505
Dannyfsp <femoscopilala@gmail.com>
2023-05-25 10:24:55 +0000

    updating existing class to accept a list of strings fields constructor argument

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ A module for filtering logs
 
 import os
 import re
+import logging
 from typing import List
 
 
@@ -16,8 +17,33 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
-        fields: List[str], redaction: str, message: str, seperator: str,
+        fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     ''' Filters a log line '''
     extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, seperator), replace(redaction), message)
+    return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ("name", "levelname", "asctime", "message")
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ formats a Log record """
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
+
+
+if __name__ == "__main__":
+    main()
