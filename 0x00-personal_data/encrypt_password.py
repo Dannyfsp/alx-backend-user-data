e6fc97ab56b2a8861642bf2b9a72ac0febdfe6ef
Dannyfsp <femoscopilala@gmail.com>
2023-05-25 11:29:04 +0000

    method that compares a hashed password and a password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''method that hashes password'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''method that compares a hashed password and a given password'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
