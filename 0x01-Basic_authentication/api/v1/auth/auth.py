30ae085f8ec4f6abc101201575f9642665e95ce5
Dannyfsp <femoscopilala@gmail.com>
2023-05-29 21:51:59 +0000

    update the require_auth function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ Authentication module to manage the API
 
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -14,7 +15,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ function that checks if a pth requires authentication
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ""
+                if exclusion_path[-1] == "*":
+                    pattern = "{}.*".format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == "/":
+                    pattern = "{}/*".format(exclusion_path[0:-1])
+                else:
+                    pattern = "{}/*".format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ method to get the authorization header field from the request
