408f8cb45e396128ed496c4afa62cad5e206192e
Dannyfsp <femoscopilala@gmail.com>
2023-05-31 19:12:19 +0000

    updating api/v1/auth/auth.py by adding a method that returns a cookie value from a request

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -7,6 +7,7 @@ Authentication module to manage the API
 from flask import request
 from typing import List, TypeVar
 import re
+import os
 
 
 class Auth:
@@ -39,3 +40,10 @@ class Auth:
         """ method that gets current user from the request
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """ Retrieves the value of the cookie named SESSION_NAME
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
