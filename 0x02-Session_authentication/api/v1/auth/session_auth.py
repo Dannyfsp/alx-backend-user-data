90f782ed901b0b0e70de2ae322823af9db03ff25
Dannyfsp <femoscopilala@gmail.com>
2023-05-31 19:33:58 +0000

    method that returns a User instance based on a cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,8 @@ Session authentication module for the API
 
 from .auth import Auth
 from uuid import uuid4
+from flask import request
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,3 +29,9 @@ class SessionAuth(Auth):
         """
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """ Retrieves the user associated with the request
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
