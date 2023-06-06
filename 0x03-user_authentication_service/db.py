60809c6a2a986bdf271ee8bd3805fc696b1b91dd
Dannyfsp <femoscopilala@gmail.com>
2023-06-06 11:50:08 +0000

    fix: add user and find user by filter methods

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,10 +5,10 @@ from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
+from typing import Dict
 
 
 class DB:
@@ -35,28 +35,17 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds a new user to the database
         """
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
+        if not email or not hashed_password:
+            return None
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs: Dict) -> User:
         """ Finds a user based on a set of filters
         """
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
-            raise NoResultFound()
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if not result:
+            raise NoResultFound
         return result
