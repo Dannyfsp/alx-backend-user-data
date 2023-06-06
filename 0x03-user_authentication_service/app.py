d4bf5ba8f7664e0671278d60922a7dc0ec8926de
Dannyfsp <femoscopilala@gmail.com>
2023-06-06 14:44:50 +0000

    simple Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A simple Flask app with user authentication features
+"""
+
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return:
+        - The home page
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
