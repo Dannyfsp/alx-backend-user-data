c12e1efdfac9fee1b462a1a44528a05d39b39227
Dannyfsp <femoscopilala@gmail.com>
2023-06-07 20:04:34 +0000

    implementing a method that gets user based on a given session ID and a method that destroys a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,6 +7,7 @@ A module for user authentication
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 from db import DB
 from user import User
@@ -68,3 +69,22 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """ Retrieves a user based on a given session ID
+        """
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroys a session associated with a given user
+        """
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
